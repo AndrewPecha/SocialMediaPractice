148b1ae9a2891bdf9a4b94078350f19351495953
Andrew Pecha <andrew.pecha@gmail.com>
2023-01-25 17:00:19 -0600

    basic controller working

---
+++ SocialMediaPractice.Api/Controllers/SocialPostController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaPractice.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SocialPostController : ControllerBase
+{
+    [HttpPost]
+    public string CreateSocialPost(string postString)
+    {
+        return postString;
+    }
+}
